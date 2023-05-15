64067f440c23a3cb17178913a97addb77edcd846
akshatha nr <akshathagupta888@gmail.com>
2023-05-16 01:30:23 +0530

    commit

---
+++ javfsd/src/pack2/AccessSpecifier4.java
@@ -0,0 +1,13 @@
+package pack2;
+import praticeproject.*;
+public class AccessSpecifier4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PublicAccessSpecifiersusingAnotherpackage obj = new PublicAccessSpecifiersusingAnotherpackage(); 
+        obj.display();  
+
+
+	}
+
+}
